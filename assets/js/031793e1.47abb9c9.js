5567715262a795918482ca556d7c0ca1c2b6ea05
Edward Zhang <643431636@qq.com>
2022-07-27 22:07:41 +0800

    update the doc

--- assets/js/031793e1.47abb9c9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs_carbonscript=self.webpackChunkdocs_carbonscript||[]).push([[1633],{2511:a=>{a.exports=JSON.parse('{"label":"facebook","permalink":"/blog/tags/facebook","allTagsPath":"/blog/tags","count":1}')}}]);
---
+++ assets/js/814f3328.fdde4ab9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs_carbonscript=self.webpackChunkdocs_carbonscript||[]).push([[2535],{5641:s=>{s.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Blank Blog","permalink":"/blog/Blank Blog"}]}')}}]);
